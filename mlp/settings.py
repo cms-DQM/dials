8f91cba18e1194f98b358fcaf045504ac57998e6
Abhit Patil <abhit95@gmail.com>
2021-10-12 12:18:32 +0200

    Fix static files

--- mlp/settings.py
+++ mlp/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'home/static'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'home/static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
