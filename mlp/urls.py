2dcf0ef0cc632c278b349ceef9678b8b2cb3a966
Abhit Patil <abhit95@gmail.com>
2021-10-17 21:34:29 +0200

    Add url to Homepage

--- mlp/urls.py
+++ mlp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("home.urls")),
+    path("datasets/", include("datasets.urls")),
     path('admin/', admin.site.urls),
 ]
