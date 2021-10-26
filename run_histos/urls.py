fcc523c10a58d97688e4d20b4e8117ad6f97f159
Xavier Coubez <xavier.coubez@cern.ch>
2021-10-26 19:16:15 +0200

    Finishing prototype of runhistos view

--- run_histos/urls.py
+++ run_histos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import chart_select_view, chart_view_altair
+from .views import run_histos_view, chart_view_altair
 
 app_name = 'run_histos'
 
 urlpatterns = [
-    path('',        chart_select_view, name='main-runhistos-view'),
+    path('',        run_histos_view, name='main-runhistos-view'),
     path('altair/', chart_view_altair, name='altair-view')
 ]
