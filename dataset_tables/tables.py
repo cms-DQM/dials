c30e45c11b5f88c0ca2df6602f83509e1bd16cf8
Abhit Patil <abhit95@gmail.com>
2021-11-08 15:57:29 +0100

    Add title to datasets in table

--- dataset_tables/tables.py
+++ dataset_tables/tables.py
@@ -4,6 +4,7 @@ from run_histos.models import RunHisto
 class RunHistoTable1D(tables.Table):
     run = tables.Column(accessor = 'run.run_number')
     primary_dataset = tables.Column(verbose_name = "Dataset")
+    title = tables.Column()
     mean = tables.Column()
     rms = tables.Column(verbose_name = "RMS")
     skewness = tables.Column()
