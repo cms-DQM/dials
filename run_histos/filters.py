04077c8e5b16c1a2136f9346476a11ac31eb30db
Abhit Patil <abhit95@gmail.com>
2021-11-22 19:59:59 +0100

    Modify filters

--- run_histos/filters.py
+++ run_histos/filters.py
@@ -8,10 +8,20 @@ class InFilter(django_filters.filters.BaseInFilter, django_filters.filters.CharF
 
 class RunHistosFilter1D(django_filters.FilterSet):
 
+    title = django_filters.filters.ModelMultipleChoiceFilter(
+        field_name="title",
+        queryset=RunHisto.objects.values_list("title", flat=True).distinct(),
+        widget=forms.SelectMultiple(attrs={
+            'class': 'form-control',
+            'size': '10',
+        })
+    )
+
     run__run_number__in = InFilter(field_name='run__run_number', lookup_expr='in')
 
     class Meta:
         model = RunHisto
         fields = {
             'run__run_number': ['gte', 'lte', ],
+            'primary_dataset': ['exact'],
         }
