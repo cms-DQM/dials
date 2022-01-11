4bb6f69bd51347618470759663f95970620cc85b
Abhit Patil <abhit95@gmail.com>
2022-01-11 05:46:10 +0530

    Add as_view

--- run_histos/urls.py
+++ run_histos/urls.py
@@ -6,7 +6,7 @@ app_name = "run_histos"
 urlpatterns = [
     path('',        run_histos_view,   name='main-runhistos-view'),
     path("listRunHistos1D/", listRunHistos1D, name="listRunHistos1D"),
-    path("listRunHistos1DAPI/", listRunHistos1DAPI, name = "listRunHistos1DAPI"),
+    path("listRunHistos1DAPI/", listRunHistos1DAPI.as_view(), name = "listRunHistos1DAPI"),
     path('import/', import_view,       name='import-view'), 
     path('altair/', altair_chart_view, name='altair-view'),
 ]
