a67a7bd81e380dcc44092a78cfe85022eabb62f8
Abhit Patil <abhit95@gmail.com>
2022-02-08 14:35:11 +0100

    Add API view for 1D histograms for lumisection type

--- lumisection_histos1D/urls.py
+++ lumisection_histos1D/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
-from .views import listLumisectionHistos1D
+from .views import listLumisectionHistos1D, listLumisectionHistos1DAPI
 
 
 app_name = "lumisection_histos1D"
 urlpatterns = [
     path("listLumisectionHistos1D/", listLumisectionHistos1D, name="listLumisectionHistos1D"),
+    path("listLumisectionHistos1DAPI/", listLumisectionHistos1DAPI.as_view(), name = "listLumisectionHistos1DAPI"),
 ]
--- lumisection_histos1D/views.py
+++ lumisection_histos1D/views.py
@@ -1,10 +1,14 @@
+import imp
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseRedirect
 from django_tables2 import RequestConfig
+from django_filters import rest_framework as filters
+from rest_framework import generics
 
 from lumisection_histos1D.models import LumisectionHisto1D
 from lumisection_histos1D.filters import LumisectionHistos1DFilter
 from lumisection_histos1D.tables import LumisectionHistos1DTable
+from lumisection_histos1D.serializers import LumisectionHisto1DSerializer
 
 import pandas as pd
 
@@ -22,3 +26,9 @@ def listLumisectionHistos1D(request):
     context["lumisectionHistos1D_table"] = lumisectionHistos1D_table
     context["filter"] = lumisectionHistos1D_filter
     return render(request, "lumisection_histos1D/listLumisectionHistos1D.html", context)
+
+class listLumisectionHistos1DAPI(generics.ListAPIView):
+    queryset = LumisectionHisto1D.objects.all()
+    serializer_class = LumisectionHisto1DSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = LumisectionHistos1DFilter
