925621abbfb241db26cc49868b85acc7aa15460c
Abhit Patil <abhit95@gmail.com>
2022-02-10 01:08:32 +0100

    Modify manage command for 2D ls histos to process by chunks

--- lumisection_histos2D/management/commands/extract_lumisections_histos2D.py
+++ lumisection_histos2D/management/commands/extract_lumisections_histos2D.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from runs.models import Run
 from lumisections.models import Lumisection
 from lumisection_histos2D.models import LumisectionHisto2D
+import json
 
 # https://betterprogramming.pub/3-techniques-for-importing-large-csv-files-into-a-django-app-2b6e5e47dba0
 import pandas as pd
@@ -16,32 +17,39 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         file_path = options["file_path"]
+        count = 0
+        for df in pd.read_csv(file_path, chunksize=50):
+            print(df.columns)
+            
+            lumisection_histos2D = []
+            count2 = 0
+            
+            for index, row in df.iterrows():
+                run_number = row["fromrun"]
+                lumi_number = row["fromlumi"]
+                title = row["hname"]
+                entries = row["entries"]
+                data=json.loads(row["histo"])
 
-        df = pd.read_csv(file_path)
-        print(df.head())
-        print(df.columns)
+                print(run_number, lumi_number, title)
 
-        lumisection_histos2D = []
+                run, _ = Run.objects.get_or_create(run_number=run_number)
+                lumisection, _ = Lumisection.objects.get_or_create(run_number=run, ls_number=lumi_number)
 
-        for index, row in df.iterrows():
-            run_number = row["fromrun"]
-            lumi_number = row["fromlumi"]
-            title = row["hname"]
-            entries = row["entries"]
+                lumisection_histo2D = LumisectionHisto2D(
+                    lumisection=lumisection,
+                    title=title,
+                    entries=entries,
+                    data=data
+                )
 
-            print(run_number, lumi_number, title)
+                lumisection_histos2D.append(lumisection_histo2D)
+                count2 += 1
+                if count2 == 10: 
+                    LumisectionHisto2D.objects.bulk_create(lumisection_histos2D, ignore_conflicts=True)
+                    print('10 2D lumisection histos 2D of chunk {} successfully added!'.format(count))
+                    count2 = 0
+                    lumisection_histos2D = []
+                    #Add case for (less than 10 cases!)
 
-            run, _ = Run.objects.get_or_create(run_number=run_number)
-            lumisection, _ = Lumisection.objects.get_or_create(run_number=run, ls_number=lumi_number)
-
-            lumisection_histo2D = LumisectionHisto2D(
-                ls_number=lumisection,
-                title=title,
-                entries=entries,
-                data=1.8
-            )
-
-            lumisection_histos2D.append(lumisection_histo2D)
-
-        LumisectionHisto2D.objects.bulk_create(lumisection_histos2D, ignore_conflicts=True)
-        print('lumisections histos 2D successfully added!')
+            count +=1
