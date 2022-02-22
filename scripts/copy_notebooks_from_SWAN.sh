fcb4a7856c0d88146bb51fbc5cbd95a8113a4875
Xavier Coubez <xavier.coubez@cern.ch>
2022-02-22 15:28:39 +0100

    Starting copy of notebooks from SWAN to respective apps

---
+++ scripts/copy_notebooks_from_SWAN.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# runs
+
+# run histos
+cp /eos/user/x/xcoubez/SWAN_projects/ml4dqm-return/loading_to_database/loading_from_ML4DQM_run_files.ipynb ../run_histos/notebooks/.
+
+# run certification
+cp /eos/user/x/xcoubez/SWAN_projects/ml4dqm-return/loading_to_database/loading_from_run_registry_run_file.ipynb ../run_certification/notebooks/.
+
+# lumisections
+
+# lumisections 1D histo
+
+# lumisections 2D histo
+
+# lumisection certification
+
