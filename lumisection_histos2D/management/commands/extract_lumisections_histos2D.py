d4dc83a66817dd2d993da22ed0e226443e41da1e
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-03-04 17:39:10 +0100

    Update 2D Lumi Histos tests and management command

--- lumisection_histos2D/management/commands/extract_lumisections_histos2D.py
+++ lumisection_histos2D/management/commands/extract_lumisections_histos2D.py
@@ -1,12 +1,5 @@
 from django.core.management.base import BaseCommand
-
-from runs.models import Run
-from lumisections.models import Lumisection
 from lumisection_histos2D.models import LumisectionHisto2D
-import json
-
-# https://betterprogramming.pub/3-techniques-for-importing-large-csv-files-into-a-django-app-2b6e5e47dba0
-import pandas as pd
 
 
 class Command(BaseCommand):
@@ -17,28 +10,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         file_path = options["file_path"]
-        count = 0
-        for df in pd.read_csv(file_path, chunksize=50):
-            lumisection_histos2D = []
-
-            for index, row in df.iterrows():
-                run_number = row["fromrun"]
-                lumi_number = row["fromlumi"]
-                title = row["hname"]
-                entries = row["entries"]
-                data=json.loads(row["histo"])
-                print(run_number, lumi_number, title)
-
-                run, _ = Run.objects.get_or_create(run_number=run_number)
-                lumisection, _ = Lumisection.objects.get_or_create(run=run, ls_number=lumi_number)
-                lumisection_histo2D = LumisectionHisto2D(
-                    lumisection=lumisection,
-                    title=title,
-                    entries=entries,
-                    data=data
-                )
-                lumisection_histos2D.append(lumisection_histo2D)
-
-            LumisectionHisto2D.objects.bulk_create(lumisection_histos2D, ignore_conflicts=True)
-            print('50 2D lumisection histos 2D of chunk {} successfully added!'.format(count))
-            count +=1
+        LumisectionHisto2D.from_file(file_path)
--- lumisection_histos2D/tests.py
+++ lumisection_histos2D/tests.py
@@ -1,3 +1,35 @@
+import os
+import os.path
+import logging
 from django.test import TestCase
+from .models import LumisectionHisto2D
+from histogram_file_manager.models import HistogramDataFile
 
-# Create your tests here.
+logger = logging.getLogger(__name__)
+
+
+class CSVHistogram2DParsingTestCase(TestCase):
+    """
+    Test parsing a 2D Histogram CSV file and storing it into the DB
+    """
+    def setUp(self):
+        # List all files in the test_files/per_lumi directory
+        test_files_directory = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "test_files",
+            "per_lumi")
+        files = [
+            os.path.join(test_files_directory, f) for f in os.listdir(
+                os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             "test_files", "per_lumi"))
+            if os.path.isfile(os.path.join(test_files_directory, f))
+        ]
+
+        # Create entries in db
+        for f in files:
+            LumisectionHisto2D.from_file(file_path=f)
+
+    def test_csv_histogram_1d_parsing(self):
+        logger.debug(f"There are {LumisectionHisto2D.objects.count()} "
+                     "2D Lumisection histograms in the DB")
+        # Stupid test to verify that 9 histograms were loaded from one file
+        assert LumisectionHisto2D.objects.count() == 9
