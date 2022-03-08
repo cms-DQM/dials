3ab8786694fbeaa785b5c496ebdbdad0a334b8c5
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-03-08 19:17:04 +0100

    Working on a pretty ugly implementation of a django_tables2 instance

--- histogram_file_manager/tables.py
+++ histogram_file_manager/tables.py
@@ -1,6 +1,7 @@
 import django_tables2 as tables
 from django.conf import settings
 from django.utils.html import format_html
+from django.core.exceptions import ObjectDoesNotExist
 from histogram_file_manager.models import HistogramDataFile
 from histogram_file_manager.forms import HistogramDataFileForm
 
@@ -13,19 +14,62 @@ class HistogramDataFileTable(tables.Table):
     filepath = tables.Column()
     in_db = tables.Column(empty_values=())
 
-    # filesize = tables.Column()
-    # dimensions = tables.Column(accessor="data_dimensionality")
-    # era = tables.Column(accessor="data_era")
-    # entries_total = tables.Column()
-    # granularity = tables.Column()
-    # percentage_processed = tables.Column()
+    filesize = tables.Column()
+    dimensionality = tables.Column(accessor="data_dimensionality")
+    era = tables.Column(accessor="data_era")
+    entries_total = tables.Column()
+    granularity = tables.Column()
+    percentage_processed = tables.Column()
 
     def render_in_db(self, value):
         return format_html(
             f"<img src='{settings.STATIC_URL}/histogram_file_manager/icons/check.svg' alt=''>"
         ) if value else ""
 
+    @classmethod
+    def from_filepaths(cls, filepaths: list):
+        """
+        Create a HistogramDataFileTable using the passed list of filepaths.
+  
+        Parameters:
+        - filepaths: a list of tuples, as returned from the forms.FilePathField class
+
+        """
+        # Get all histograms stored in db
+        files_in_db = HistogramDataFile.objects.all()
+
+        # Ignore some fields, or override them
+        overridden_fields = [
+            'filepath', 'lumisectionhisto1d', 'lumisectionhisto2d'
+        ]
+
+        # Data that will be returned to be rendered as a table
+        new_table_data = []
+
+        # For each filepath passed as argument
+        for f in filepaths:
+            new_table_entry = {'filepath': f[0]}
+            # If this succeeds, the filepath has already been recorded in db
+            try:
+                entry = files_in_db.get(filepath=f[0])
+                # Copy the values from each fields to the new table entry
+                for field in HistogramDataFile._meta.get_fields():
+                    if field.name not in overridden_fields:
+                        new_table_entry[field.name] = getattr(
+                            entry, field.name)
+                new_table_entry['in_db'] = True
+
+            # File not stored in db, display empty data
+            except ObjectDoesNotExist as e:
+                new_table_entry['in_db'] = False
+
+            new_table_data.append(new_table_entry)
+
+        # Cross-check if the passed filepaths are stored into the
+        # database, and fill the columns with existing data
+        return cls(new_table_data)
+
     class Meta:
         #
-        # model = HistogramDataFile
+        model = HistogramDataFile
         fields = []
--- histogram_file_manager/views.py
+++ histogram_file_manager/views.py
@@ -14,16 +14,9 @@ def histogram_file_manager(request):
     """
     context = {}
     all_files = HistogramDataFileForm().fields.get('filepath')._choices
-    files_in_db = HistogramDataFile.objects.all()
 
     # Populate the table with data
-    table = HistogramDataFileTable([{
-        "filepath":
-        f[0],
-        "in_db":
-        bool(len(files_in_db.filter(filepath=f[0])) == 1)
-    } for f in all_files])
-    context['files_in_db'] = files_in_db
+    table = HistogramDataFileTable.from_filepaths(all_files)
     context['table'] = table
 
     # Populate list of available DQM histogram files
