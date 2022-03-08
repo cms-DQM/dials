6766930f4bed655db0a1f18280766422100f912b
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-03-08 18:47:11 +0100

    Simple HistogramDataFile scan test

--- histogram_file_manager/tests.py
+++ histogram_file_manager/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.conf import settings
+from histogram_file_manager.forms import HistogramDataFileForm
+from histogram_file_manager.modeils import HistogramDataFile
 
-# Create your tests here.
+
+class HistogramDataFileTestCase(TestCase):
+    def setUp(self):
+        """
+        Get the _choices list from the HistogramDataFileForm which
+        will populate the default directory setup in the models (DIR_PATH_EOS_CMSML4DC).
+        """
+        self.all_files = [
+            f[0]
+            for f in HistogramDataFileForm().fields.get('filepath')._choices
+        ]
+
+    def test_data_file_detection(self):
+        """
+        There should be at least one file in this directory when testing.
+        """
+        assert len(self.all_files) > 0
