ca84ce4e390d359d81d9f929cd36b31a3187477c
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-03-22 12:13:56 +0100

    Formatting

--- lumisection_histos1D/views.py
+++ lumisection_histos1D/views.py
@@ -12,20 +12,26 @@ from lumisection_histos1D.serializers import LumisectionHisto1DSerializer
 
 import pandas as pd
 
+
 def listLumisectionHistos1D(request):
     """
     View to list the filtered 1D histograms for Lumisections
     """
     context = {}
     lumisectionHistos1D_list = LumisectionHisto1D.objects.all()
-    lumisectionHistos1D_filter = LumisectionHistos1DFilter(request.GET, queryset=lumisectionHistos1D_list)
-    lumisectionHistos1D_table = LumisectionHistos1DTable(lumisectionHistos1D_filter.qs[:50])
+    lumisectionHistos1D_filter = LumisectionHistos1DFilter(
+        request.GET, queryset=lumisectionHistos1D_list
+    )
+    lumisectionHistos1D_table = LumisectionHistos1DTable(
+        lumisectionHistos1D_filter.qs[:50]
+    )
 
     RequestConfig(request).configure(lumisectionHistos1D_table)
 
     context["lumisectionHistos1D_table"] = lumisectionHistos1D_table
     context["filter"] = lumisectionHistos1D_filter
     return render(request, "lumisection_histos1D/listLumisectionHistos1D.html", context)
+
 
 class listLumisectionHistos1DAPI(generics.ListAPIView):
     queryset = LumisectionHisto1D.objects.all()
