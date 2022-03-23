1e5c5f17c94f0ac7a221773de609b9226a2da8aa
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-03-23 14:11:46 +0100

    Removed 2d histograms app

--- lumisection_histos2D/api/routers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import routers
-from lumisection_histos2D.api.viewsets import LumisectionHisto2DViewset
-
-router = routers.SimpleRouter()
-router.register(r'lumisection_histos2D', LumisectionHisto2DViewset)
--- lumisection_histos2D/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import listLumisectionHistos2D
-
-app_name = "lumisection_histos2D"
-
-urlpatterns = [
-    path("list/", listLumisectionHistos2D, name="list"),
-]
