e6f7be2e272a1f39921aa4445c1996ba32ea1537
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-04-08 14:09:43 +0200

    Use ASGI, added missing css

--- mlp/settings.py
+++ mlp/settings.py
@@ -92,8 +92,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "mlp.wsgi.application"
-# ASGI_APPLICATION = "mlp.asgi.application"
+# WSGI_APPLICATION = "mlp.wsgi.application"
+ASGI_APPLICATION = "mlp.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
