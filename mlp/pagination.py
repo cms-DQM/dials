ef99ef58493ed4e5bb3078d5a409a69eaa05ded6
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-04-12 15:01:20 +0200

    Make custom paginator return an ordered dict

--- mlp/pagination.py
+++ mlp/pagination.py
@@ -1,7 +1,7 @@
 """
 Custom pagination function to return relative links
 """
-
+from collections import OrderedDict
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
@@ -12,20 +12,21 @@ class MLPlaygroundAPIPagination(PageNumberPagination):
         nl = self.get_next_link()
         pl = self.get_previous_link()
 
-        return Response({
-            'next':
-            nl[nl.find("/api"):] if nl is not None else None,
-            'previous':
-            pl[pl.find("/api"):] if pl is not None else None,
+        return Response(
+            OrderedDict({
+                'next':
+                nl[nl.find("/api"):] if nl is not None else None,
+                'previous':
+                pl[pl.find("/api"):] if pl is not None else None,
 
-            # 'current_page':
-            # int(self.request.query_params.get('page', 1)),
-            'total':
-            self.page.paginator.count,
-            'per_page':
-            self.page_size,
-            'total_pages':
-            round(self.page.paginator.count / self.page_size, 1),
-            'results':
-            data,
-        })
+                # 'current_page':
+                # int(self.request.query_params.get('page', 1)),
+                'total':
+                self.page.paginator.count,
+                'per_page':
+                self.page_size,
+                'total_pages':
+                round(self.page.paginator.count / self.page_size, 1),
+                'results':
+                data,
+            }))
