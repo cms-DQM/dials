a8be3914803dc7c9ea1723e2f54122468ee5f005
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-04-12 11:09:40 +0200

    Timing serialization

--- histogram_file_manager/api/serializers.py
+++ histogram_file_manager/api/serializers.py
@@ -1,11 +1,35 @@
+import time
+import logging
 from rest_framework import serializers
 from histogram_file_manager.models import HistogramDataFile
 
+logger = logging.getLogger(__name__)
 
 class HistogramDataFileSerializer(serializers.ModelSerializer):
-    # percentage_processed = serializers.DecimalField(max_digits=10,
-    # decimal_places=1)
+    percentage_processed = serializers.DecimalField(max_digits=10,
+                                                    decimal_places=1)
+
+    def to_representation(self, instance, *args, **kwargs):
+        start = time.time()
+        a = super().to_representation(instance, *args, **kwargs)
+        logger.debug(f"Serialization took {time.time() - start:.6f}")
+        return a
 
     class Meta:
         model = HistogramDataFile
         fields = '__all__'
+
+
+class HistogramDataFileSerializer_(serializers.Serializer):
+    id = serializers.IntegerField()
+    filepath = serializers.CharField()
+    filesize = serializers.FloatField()
+    data_dimensionality = serializers.IntegerField()
+    data_era = serializers.CharField()
+    entries_total = serializers.IntegerField()
+    entries_processed = serializers.IntegerField()
+    granularity = serializers.CharField()
+    created = serializers.DateTimeField()
+    modified = serializers.DateTimeField()
+    percentage_processed = serializers.DecimalField(max_digits=10,
+                                                    decimal_places=1)
