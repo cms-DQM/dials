a40fab8508c67897aba03fc6ba96e818ead42dce
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-04-14 17:42:26 +0200

    Fixed discover_dqm_files to filter out invalid extensions

--- histogram_file_manager/management/commands/discover_dqm_files.py
+++ histogram_file_manager/management/commands/discover_dqm_files.py
@@ -1,3 +1,4 @@
+import re
 import os
 import logging
 from django.core.management.base import BaseCommand
@@ -9,14 +10,36 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = ("Scans the default DQM datafile directory for new files and"
-            "stores them in the Database")
+    help = (
+        "Scans the default DQM datafile directory recursively for new files and"
+        "stores them in the Database")
 
     def handle(self, *args, **options):
+        valid_files_extensions = [
+            choice[0] for choice in HistogramDataFile.DATAFILE_FORMAT_CHOICES
+        ]
+
+        logger.debug(
+            f"Getting recursive file list of directory {settings.DIR_PATH_EOS_CMSML4DC} ..."
+        )
         for root, dirs, files in sorted(os.walk(
                 settings.DIR_PATH_EOS_CMSML4DC)):
+
             for f in sorted(files):
+                # Check if file has correct extension
+                is_valid_extension = False
+                for valid_extension in valid_files_extensions:
+                    if f.endswith(valid_extension):
+                        is_valid_extension = True
+                        break
+
+                if not is_valid_extension:
+                    logger.debug(
+                        f"Invalid file extension on file {f}, skipping")
+                    continue
+
                 f = os.path.join(root, f)
+
                 try:
                     hdf = HistogramDataFile.objects.get(filepath=f)
                     logger.debug(f"File '{hdf}' already in DB")
