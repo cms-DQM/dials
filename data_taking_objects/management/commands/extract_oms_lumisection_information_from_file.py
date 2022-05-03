12cc2cc49a6f4ba1c48499052a87f56aad1644e5
Xavier Coubez <xavier.coubez@cern.ch>
2022-05-03 14:34:47 +0200

    Moving to bulk creation and bulk update - currently with pk error

--- data_taking_objects/management/commands/extract_oms_lumisection_information_from_file.py
+++ data_taking_objects/management/commands/extract_oms_lumisection_information_from_file.py
@@ -20,28 +20,57 @@ class Command(BaseCommand):
 
         df_rate = pd.read_pickle(file_path)
 
-        print(df_rate.head())
+        name_mapping = {
+            "attributes.run_number": "run_number",
+            "attributes.first_lumisection_number": "lumisection_number",
+            "attributes.rate": "rate",
+        }
 
-        # VERY VERY SLOW
-        # TODO
-        # move to a loop on run numbers and update or create all the lumisections
-        for index, row in df_rate.iterrows():
+        df_rate = (
+            df_rate[
+                [
+                    "attributes.run_number",
+                    "attributes.first_lumisection_number",
+                    "attributes.rate",
+                ]
+            ]
+            .copy()
+            .rename(columns=name_mapping)
+        )
 
-            run_number = row["attributes.run_number"]
-            ls_number = row["attributes.first_lumisection_number"]
-            rate = row["attributes.rate"]
+        list_of_runs = sorted(df_rate.run_number.unique())
+        print(list_of_runs)
 
-            print(run_number, ls_number, rate)
+        for run_number in list_of_runs:
 
             run, _ = Run.objects.get_or_create(run_number=run_number)
-            lumisection, _ = Lumisection.objects.update_or_create(
-                run=run, ls_number=ls_number, oms_zerobias_rate=rate
+
+            list_of_lumisections = sorted(
+                df_rate.query("run_number==@run_number").lumisection_number.unique()
+            )
+            print(list_of_lumisections)
+
+            lumisections_to_create_or_update = []
+
+            for lumisection_number in list_of_lumisections:
+                rate = df_rate.query(
+                    "(run_number==@run_number)&(lumisection_number==@lumisection_number)"
+                ).rate.values[0]
+
+                print(f"Adding {run_number} / {lumisection_number} / {rate} to the DB")
+
+                lumisection = Lumisection(
+                    run=run, ls_number=lumisection_number, oms_zerobias_rate=rate
+                )
+
+                lumisections_to_create_or_update.append(lumisection)
+
+            print("Trying bulk creation of objects")
+            Lumisection.objects.bulk_create(
+                lumisections_to_create_or_update, ignore_conflicts=True
             )
 
-            # alternatively
-            # lumisection = Lumisection(...)
-            # lumisections.append(lumisection)
-            # Lumisection.objects.bulk_create(lumisections)
-            # would require bulk_update_or_create...
-
-            print(f"run {run_number} / ls {ls_number} rate successfully added!")
+            print("Trying bulk update of objects")
+            Lumisection.objects.bulk_update(
+                lumisections_to_create_or_update, ["oms_zerobias_rate"]
+            )
