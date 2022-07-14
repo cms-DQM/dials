888ecd8afdeba72fccefec91a654faa965cb3866
Dimitris Papagiannis <nothingface0@gmail.com>
2022-07-14 17:16:00 +0200

    Added ListView for Tasks

--- challenge/urls.py
+++ challenge/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from challenge.views import (
-    create_task_view,
-)
+from challenge.views import create_task_view, TaskListView
 
 app_name = "challenge"
 
 urlpatterns = [
     path("create_task/", create_task_view, name="create-task-view"),
+    path("task_list/", TaskListView.as_view(), name="task-list"),
 ]
--- challenge/views.py
+++ challenge/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from challenge.models import Task
+from django.views.generic.list import ListView
 from challenge.forms import TaskForm
+from challenge.models import Task
 
 # TODO Check that this is the best option
-
-
 def create_task_view(request):
+    """
+    View for creating Task instances from
+    list of testing runs and testing lumisections.
+    """
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
@@ -15,3 +18,7 @@ def create_task_view(request):
     else:
         form = TaskForm()
         return render(request, "challenge/create_task.html", {"form": form})
+
+
+class TaskListView(ListView):
+    model = Task
