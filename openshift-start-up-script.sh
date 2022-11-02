6ac50b071f1fb4b548bf685815214095837b44f7
Dimitris Papagiannis <nothingface0@gmail.com>
2022-11-02 09:40:50 +0100

    Add ROOT to path on startup

--- openshift-start-up-script.sh
+++ openshift-start-up-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python manage.py collectstatic --noinput
-# python manage.py migrate --run-syncdb
+# python manage.py migrate --run-syncdb # If you want migrations to run at app start
 
-daphne -b 0.0.0.0 -p 8080 --application-close-timeout 60 mlp.asgi:application
-# gunicorn --bind=0.0.0.0:8080 mlp.wsgi
-# python manage.py runserver 0.0.0.0:8080
+source /opt/app-root/src/root/bin/thisroot.sh # Add ROOT to PATH
+
+daphne -b 0.0.0.0 -p 8080 --application-close-timeout 60 mlp.asgi:application # Start Django
