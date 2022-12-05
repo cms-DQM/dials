d0930a450727dc33021609333ef479b985b4e76a
Dimitris Papagiannis <d.papag.a@gmail.com>
2022-12-05 10:04:18 +0100

    Add ROOT to path before doing anything

--- openshift-start-up-script.sh
+++ openshift-start-up-script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+source /opt/app-root/src/root/bin/thisroot.sh # Add ROOT to PATH
+
 python manage.py collectstatic --noinput
 # python manage.py migrate --run-syncdb # If you want migrations to run at app start
 
-source /opt/app-root/src/root/bin/thisroot.sh # Add ROOT to PATH
 
 daphne -b 0.0.0.0 -p 8080 --application-close-timeout 60 mlp.asgi:application # Start Django
