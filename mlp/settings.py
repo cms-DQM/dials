d93c220bccfe7855c7c83120ea13758e31ba0f56
Vichayanun Wachirapusitanand <vichayanun.wachirapusitanand@cern.ch>
2023-09-12 23:03:47 +0700

    Added MIDDLEWARE as complained by Openshift

--- mlp/settings.py
+++ mlp/settings.py
@@ -81,6 +81,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "mlp.urls"
