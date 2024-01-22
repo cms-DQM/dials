936251f29929f3bbb429deb4a77df31b27c283b7
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-01-22 13:59:17 +0100

    - refactor: use Image component from react-boostrap instead of base tag <img> in home carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
 
 import RawImg from '../assets/img/AnomalyDetection.jpg';
 import AnomalyDetectedImg from '../assets/img/AnomalyDetectionV2.jpg';
@@ -10,18 +11,10 @@ const Home = () => {
     <Container>
       <Carousel fade controls={false} indicators={false} pause={false} interval={1000}>
         <Carousel.Item>
-          <img
-            src={RawImg}
-            className="d-block pt-5 img-fluid mx-auto w-100"
-            alt="Raw object"
-          />
+          <Image src={RawImg} className="d-block pt-5 img-fluid mx-auto w-100" alt="Raw object"></Image>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            src={AnomalyDetectedImg}
-            className="d-block pt-5 img-fluid mx-auto w-100"
-            alt="Anomaly detected!"
-          />
+          <Image src={AnomalyDetectedImg} className="d-block pt-5 img-fluid mx-auto w-100" alt="Anomaly detected!"></Image>
         </Carousel.Item>
       </Carousel>
     </Container>
