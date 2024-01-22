db6b01e4dad603923e84182aa19acfef6fd13cd0
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-01-22 13:31:04 +0100

    - feat: add homepage - feat: add navbar using react-bootstrap and react-router-dom

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Carousel } from 'react-bootstrap';
+
+import RawImg from '../assets/img/AnomalyDetection.jpg';
+import AnomalyDetectedImg from '../assets/img/AnomalyDetectionV2.jpg';
+
+const Home = () => {
+  return (
+    <Container>
+      <Carousel fade controls={false} indicators={false} pause={false} interval={1000}>
+        <Carousel.Item>
+          <img
+            src={RawImg}
+            className="d-block pt-5 img-fluid mx-auto w-100"
+            alt="Raw object"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src={AnomalyDetectedImg}
+            className="d-block pt-5 img-fluid mx-auto w-100"
+            alt="Anomaly detected!"
+          />
+        </Carousel.Item>
+      </Carousel>
+      </Container>
+  )
+};
+
+export default Home;
