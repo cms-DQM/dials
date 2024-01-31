e4ee0ed090522cbd4d3ad8bdd5ddbc8dc1bfebe9
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-01-31 17:51:10 +0100

    feat: add ls_number and run_number as output fields in H1D and H2D serializers using custom serializer `ExtraFieldsSerializer`

--- backend/mlplayground/dqmio_etl/serializers.py
+++ backend/mlplayground/dqmio_etl/serializers.py
@@ -3,6 +3,16 @@ from rest_framework import serializers
 from .models import Lumisection, LumisectionHistogram1D, LumisectionHistogram2D, Run
 
 
+class ExtraFieldsSerializer(serializers.ModelSerializer):
+    def get_field_names(self, declared_fields, info):
+        expanded_fields = super(ExtraFieldsSerializer, self).get_field_names(declared_fields, info)
+
+        if getattr(self.Meta, "extra_fields", None):
+            return expanded_fields + self.Meta.extra_fields
+        else:
+            return expanded_fields
+
+
 class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
@@ -15,16 +25,24 @@ class LumisectionSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class LumisectionHistogram1DSerializer(serializers.ModelSerializer):
+class LumisectionHistogram1DSerializer(ExtraFieldsSerializer):
+    ls_number = serializers.IntegerField(source="lumisection.ls_number")
+    run_number = serializers.IntegerField(source="lumisection.run.run_number")
+
     class Meta:
         model = LumisectionHistogram1D
         fields = "__all__"
+        extra_fields = ["ls_number", "run_number"]
 
 
-class LumisectionHistogram2DSerializer(serializers.ModelSerializer):
+class LumisectionHistogram2DSerializer(ExtraFieldsSerializer):
+    ls_number = serializers.IntegerField(source="lumisection.ls_number")
+    run_number = serializers.IntegerField(source="lumisection.run.run_number")
+
     class Meta:
         model = LumisectionHistogram2D
         fields = "__all__"
+        extra_fields = ["ls_number", "run_number"]
 
 
 class LumisectionHistogramsIngetionInputSerializer(serializers.Serializer):
