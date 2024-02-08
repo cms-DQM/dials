c450c41badb2447ef128f5fd8a053d42fc8de641
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-02-08 22:51:47 +0100

    refactor: modify dev server start script specifying host and port

--- backend/scripts/start-dev.sh
+++ backend/scripts/start-dev.sh
@@ -8,7 +8,7 @@ poetry run celery -A mlplayground worker -l INFO -c 1 -n worker2 -Q dqmio_etl_qu
 P2=$!
 poetry run celery -A mlplayground beat -l INFO &
 P3=$!
-poetry run python manage.py runserver
+poetry run python manage.py runserver 0.0.0.0:8000
 P4=$!
 
 wait $P1 $P2 $P3 $P4
