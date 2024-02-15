7b4c406ef4940d9a27abb866feec4286adfa0e89
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-02-15 21:36:48 +0100

    fix: allow computed elapsed time to be None in `CeleryTasksSerializer`

--- backend/mlplayground/dqmio_celery_tasks/serializers.py
+++ backend/mlplayground/dqmio_celery_tasks/serializers.py
@@ -23,7 +23,9 @@ class TaskResponseSerializer(serializers.Serializer):
 
 
 class CeleryTasksSerializer(serializers.ModelSerializer):
-    elapsed_time = serializers.SerializerMethodField(method_name="compute_elapsed_time")
+    elapsed_time = serializers.SerializerMethodField(
+        method_name="compute_elapsed_time", required=False, allow_null=True
+    )
 
     class Meta:
         model = TaskResult
@@ -32,6 +34,8 @@ class CeleryTasksSerializer(serializers.ModelSerializer):
 
     @extend_schema_field(serializers.IntegerField)
     def compute_elapsed_time(self, obj) -> int:
+        if obj.status == states.PENDING:
+            return None
         is_ready = obj.status in states.READY_STATES
         ftime = ftime = obj.date_done if is_ready else timezone.make_aware(datetime.now(), APP_TZ)
         return (ftime - obj.date_created).total_seconds()
