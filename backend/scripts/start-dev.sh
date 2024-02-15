9173bcce2b2fedd194139f8495df4cd82cefff93
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-02-15 16:20:09 +0100

    chore: add worker3 process to docker-compose files and start-dev script

--- backend/scripts/start-dev.sh
+++ backend/scripts/start-dev.sh
@@ -6,9 +6,11 @@ poetry run celery -A mlplayground worker -l INFO -c 1 -n worker1 -Q dqmio_file_i
 P1=$!
 poetry run celery -A mlplayground worker -l INFO -c 1 -n worker2 -Q dqmio_etl_queue &
 P2=$!
-poetry run celery -A mlplayground beat -l INFO &
+poetry run celery -A mlplayground worker -l INFO -n worker3 -Q celery_periodic &
 P3=$!
-poetry run python manage.py runserver 0.0.0.0:8000
+poetry run celery -A mlplayground beat -l INFO &
 P4=$!
+poetry run python manage.py runserver 0.0.0.0:8000
+P5=$!
 
-wait $P1 $P2 $P3 $P4
+wait $P1 $P2 $P3 $P4 $P5
