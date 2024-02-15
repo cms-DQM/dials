cde6fd73b35c56c7853f2ae0dcc633e379881198
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-02-15 15:09:35 +0100

    refactor: remove `search_files_and_index` action from `FileIndexViewSet` (indexing will be controlled only by the scheduler)

--- backend/mlplayground/dqmio_file_indexer/viewsets.py
+++ backend/mlplayground/dqmio_file_indexer/viewsets.py
@@ -2,16 +2,11 @@ import logging
 
 from custom_auth.keycloak import KeycloakAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
-from dqmio_celery_tasks.serializers import TaskResponseBase, TaskResponseSerializer
-from drf_spectacular.utils import extend_schema
 from rest_framework import mixins, viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 from .filters import FileIndexFilter
 from .models import FileIndex
 from .serializers import FileIndexSerializer
-from .tasks import index_raw_data
 
 logger = logging.getLogger(__name__)
 
@@ -22,17 +17,3 @@ class FileIndexViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewset
     filter_backends = [DjangoFilterBackend]
     filterset_class = FileIndexFilter
     authentication_classes = [KeycloakAuthentication]
-
-    @extend_schema(request=None, responses={200: TaskResponseSerializer})
-    @action(
-        detail=False,
-        methods=["post"],
-        name="Search files in DQMIO storages and index",
-        url_path=r"ingest",
-        pagination_class=None,
-    )
-    def search_files_and_index(self, request):
-        task = index_raw_data.delay()
-        task = TaskResponseBase(id=task.id, state=task.state, ready=task.ready())
-        task = TaskResponseSerializer(task)
-        return Response(task.data)
