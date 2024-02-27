2feb17e9a8fd2a5bbaabe782344502fea70247d4
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-02-27 20:08:47 +0100

    refactor: remove chess carousel from homepage and add static background image of PixelPhase1/Tracks/PXBarrel* 2d plots

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -1,35 +1,14 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Carousel from 'react-bootstrap/Carousel'
 import Image from 'react-bootstrap/Image'
 
-import RawImg from '../assets/img/AnomalyDetection.jpg'
-import AnomalyDetectedDogImg from '../assets/img/AnomalyDetectionDog.jpg'
-import AnomalyDetectedPonyImg from '../assets/img/AnomalyDetectionPony.jpg'
-import AnomalyDetectedPersonImg from '../assets/img/AnomalyDetectionPerson.jpg'
-import AnomalyDetectedHiggsImg from '../assets/img/AnomalyDetectionHiggs.jpg'
+import background from '../assets/img/background.png'
 
 const Home = () => {
   return (
-    <Container>
-      <Carousel fade controls={false} indicators={false} pause={false} interval={3000}>
-        <Carousel.Item>
-          <Image src={RawImg} className='d-block pt-5 img-fluid mx-auto w-100' alt='Raw object'></Image>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Image src={AnomalyDetectedDogImg} className='d-block pt-5 img-fluid mx-auto w-100' alt='Dog anomaly detected!'></Image>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Image src={AnomalyDetectedPonyImg} className='d-block pt-5 img-fluid mx-auto w-100' alt='Pony anomaly detected!'></Image>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Image src={AnomalyDetectedPersonImg} className='d-block pt-5 img-fluid mx-auto w-100' alt='Person anomaly detected!'></Image>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Image src={AnomalyDetectedHiggsImg} className='d-block pt-5 img-fluid mx-auto w-100' alt='Higgs anomaly detected!'></Image>
-        </Carousel.Item>
-      </Carousel>
-    </Container>
+    <Image
+    style={{ opacity: 1, position: 'fixed', top: 0, left: 0, 'min-width': '100%', 'min-height': '100%' }}
+    src={background} alt=''
+  />
   )
 }
 
