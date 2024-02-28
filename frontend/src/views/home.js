b58c3f111b7c6d2a9de1ef1c072f2ad28d7a417e
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-02-28 12:09:16 +0100

    refactor: remove static background image and add DIALS logo and explain text in the home page

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -1,14 +1,53 @@
 import React from 'react'
 import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
-import background from '../assets/img/background.png'
+import logo from '../assets/img/logo.png'
 
 const Home = () => {
   return (
-    <Image
-    style={{ opacity: 1, position: 'fixed', top: 0, left: 0, 'min-width': '100%', 'min-height': '100%' }}
-    src={background} alt=''
-  />
+    <Container>
+      <Row className='mt-5 mb-3 align-items-center'>
+        <Col md={4} className='text-center'>
+          <Image
+            src={logo}
+            alt=''
+          />
+        </Col>
+        <Col md={8}>
+          <Card>
+            <Card.Body>
+              <h3>DIALS: Data Inspector for Anomalous Lumi-Sections</h3>
+              <br/>
+              {`
+              During a Run, the CMS Experiment collects particle collision data in Lumisection time-frame and the experiment
+              subsystem health is monitored by shifters using DQMGUI. Multiple shifters for many subsystems monitor the most
+              recent lumisection of the current run plots to check a subsystem\'s health, but given the number of plots and
+              the limited manpower it is not possible to closely pay attention to all plots during the lumisection time-frame.
+              On the other hand, using certified data it is possible to extract knowledge from old runs (at lumisection-level) to
+              help shifters get a glimpse of detector\'s health on past runs.
+              `}
+              <br/>
+              <br/>
+              {`
+              DIALS is an application designed to be an access point to DQMIO per-LS monitoring elements.
+              It is responsible for indexing, storing pre-processed DQMIO data and serving via a WEB UI and
+              REST Api, so that it could be used by any CMS sub-group for exploratory analysis, statistical learning and machine learning.
+              `}
+              <br/>
+              <br/>
+              {`
+              This application was born in CMS Tracker ML as a prototype under the name "MLPlayground" and
+              was centralized in CMS DQM-DC to be a common access point for every system.
+              `}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row >
+    </Container>
   )
 }
 
