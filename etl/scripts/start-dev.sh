d2884f2fd0b5882e51b89bb62a521df3a4d1c8ff
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-04-10 14:11:55 +0200

    chore: improve etl start-dev script to be configurable depending on workspaces set in environment

--- etl/scripts/start-dev.sh
+++ etl/scripts/start-dev.sh
@@ -6,44 +6,40 @@ if [ "$(basename $CURR_PATH)" != "etl" ]; then
     cd "etl" || exit 1
 fi
 
-celery --app=python beat --loglevel=INFO -S redbeat.RedBeatScheduler &
-PID_1=$!
+# Source .env
+if [ -f .env ]; then
+    set -a
+    source .env
+    set +a
+else
+    echo "File .env not found in $CURR_PATH"
+    exit 1
+fi
 
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=csc-indexer@%h --queues=indexer-csc &
-PID_2=$!
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=csc-bulk@%h --queues=bulk-csc &
-PID_3=$!
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=csc-priority@%h --queues=priority-csc &
-PID_4=$!
+# Parse databases set in environment
+databases_parsed=$(echo $DATABASES | sed 's/[ ][ ]*//g')
+IFS=',' read -r -a db_names <<< "$databases_parsed"
 
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=ecal-indexer@%h --queues=indexer-ecal &
-PID_5=$!
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=ecal-bulk@%h --queues=bulk-ecal &
-PID_6=$!
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=ecal-priority@%h --queues=priority-ecal &
-PID_7=$!
+# Array of pids to wait
+pids_arr=()
 
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=hcal-indexer@%h --queues=indexer-hcal &
-PID_8=$!
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=hcal-bulk@%h --queues=bulk-hcal &
-PID_9=$!
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=hcal-priority@%h --queues=priority-hcal &
-PID_10=$!
+# Always use the scheduler
+poetry run celery --app=python beat --loglevel=INFO -S redbeat.RedBeatScheduler &
+pids_arr+=($!)
 
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=tracker-indexer@%h --queues=indexer-tracker &
-PID_11=$!
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=tracker-bulk@%h --queues=bulk-tracker &
-PID_12=$!
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=tracker-priority@%h --queues=priority-tracker &
-PID_13=$!
+# Start workers for each workspace
+for db_name in "${db_names[@]}"; do
+    poetry run celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=${db_name}-indexer@%h --queues=${db_name}-indexer &
+    pids_arr+=($!)
+    poetry run celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=${db_name}-bulk@%h --queues=${db_name}-bulk &
+    pids_arr+=($!)
+    poetry run celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=${db_name}-priority@%h --queues=${db_name}-priority &
+    pids_arr+=($!)
+done
 
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=jetmet-indexer@%h --queues=indexer-jetmet &
-PID_14=$!
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=jetmet-bulk@%h --queues=bulk-jetmet &
-PID_15=$!
-celery --app=python worker --loglevel=INFO --concurrency=1 --autoscale=1,0 --max-tasks-per-child=1 --hostname=jetmet-priority@%h --queues=priority-jetmet &
-PID_16=$!
-celery -A python flower
-PID_17=$!
+# Start Flower for debugging
+poetry run celery --app=python flower
+pids_arr+=($!)
 
-wait $PID_1 $PID_2 $PID_3 $PID_4 $PID_5 $PID_6 $PID_7 $PID_8 $PID_9 $PID_10 $PID_11 $PID_12 $PID_13 $PID_14 $PID_15 $PID_16 $PID_17
+# Wait for all pids
+wait ${pids_arr[*]}
