954f0452509fd983a1996e0fdf8dbc4df277b09e
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-05-01 00:38:34 +0200

    fix: dataset_indexer should send dataset_access_type="*" to DBS api to list all possible datasets

--- etl/python/pipelines/dataset_indexer/extract.py
+++ etl/python/pipelines/dataset_indexer/extract.py
@@ -6,4 +6,4 @@ from ...env import cert_fpath, key_fpath
 def extract(primary_dataset: str) -> list:
     dbs = MinimalDBSClient(cert_fpath, key_fpath)
     dt_pattern = f"/{primary_dataset}/{era_cmp_pattern}/DQMIO"
-    return dbs.get(endpoint="datasets", params={"dataset": dt_pattern, "detail": 1})
+    return dbs.get(endpoint="datasets", params={"dataset": dt_pattern, "detail": 1, "dataset_access_type": "*"})
