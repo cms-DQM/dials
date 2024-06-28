5b9dbdc5cbcae5cf84a16d045b8f9ed21e52b407
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-06-28 13:45:24 +0200

    refactor: update dataset_indexer's extract with dbs_instance and dbs_pattern from primary_dataset object

--- etl/python/pipelines/dataset_indexer/extract.py
+++ etl/python/pipelines/dataset_indexer/extract.py
@@ -1,9 +1,9 @@
 from ...common.dbs_client import MinimalDBSClient
-from ...config import era_cmp_pattern
 from ...env import cert_fpath, key_fpath
 
 
-def extract(primary_dataset: str) -> list:
-    dbs = MinimalDBSClient(cert_fpath, key_fpath)
-    dt_pattern = f"/{primary_dataset}/{era_cmp_pattern}/DQMIO"
-    return dbs.get(endpoint="datasets", params={"dataset": dt_pattern, "detail": 1, "dataset_access_type": "*"})
+def extract(primary_dataset: dict) -> list:
+    dbs = MinimalDBSClient(primary_dataset["dbs_instance"], cert_fpath, key_fpath)
+    return dbs.get(
+        endpoint="datasets", params={"dataset": primary_dataset["dbs_pattern"], "detail": 1, "dataset_access_type": "*"}
+    )
