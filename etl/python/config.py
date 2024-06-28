fc5f6523a120528aa466652096132a54ad6cc5f0
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-06-28 16:43:25 +0200

    refactor: pd_queues object generation, downloader queues are hardcoded into the etl.config.json

--- etl/python/config.py
+++ etl/python/config.py
@@ -16,26 +16,16 @@ with open(etl_config_fpath) as f:
 common_chunk_size = config_contents["common_chunk_size"]
 common_indexer_queue = config_contents["common_indexer_queue"]
 dev_env_label = config_contents["dev_env_label"]
-downloader_bulk_suffix = config_contents["downloader_bulk_suffix"]
-downloader_priority_suffix = config_contents["downloader_priority_suffix"]
 priority_era = config_contents["priority_era"]
 th1_types = config_contents["th1_types"]
 th2_types = config_contents["th2_types"]
 th2_chunk_size = config_contents["th2_chunk_size"]
 workspaces = config_contents["workspaces"]
 
-# List all primary datasets (removing duplicates) and generate queue names
+# List all primary datasets (removing duplicates)
 primary_datasets = [obj for ws in workspaces for obj in ws["primary_datasets"]]
-primary_datasets = {(d["name"], d["dbs_pattern"], d["dbs_instance"]): d for d in primary_datasets}.values()
+primary_datasets = {(d["dbs_pattern"], d["dbs_instance"]): d for d in primary_datasets}.values()
 primary_datasets = sorted(primary_datasets, key=lambda x: x["name"])
-unique_pds = sorted(set([pd["name"] for pd in primary_datasets]))
-pds_queues = {
-    primary_dataset: {
-        "bulk_queue": f"{primary_dataset}{downloader_bulk_suffix}",
-        "priority_queue": f"{primary_dataset}{downloader_priority_suffix}",
-    }
-    for primary_dataset in unique_pds
-}
 
 # We can delete config_contents from memory
 del config_contents
