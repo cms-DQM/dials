5a54bf41713d47f3da5882bab4506e3a6c22f758
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-07-31 13:49:27 +0200

    fix: auto resize plots generated inside 0 height div
    - ResponsivePlots rendered inside `lumisection` page Accordion gets
    rendered with wrong initial height and width since accordion items body
    is initially hidden. Adding `react-resize-detector` the plot container
    geits automatically scaled when the accordion item is expanded.

--- frontend/src/components/responsivePlot.jsx
+++ frontend/src/components/responsivePlot.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 
 import Plot from 'react-plotly.js'
+import { useResizeDetector } from 'react-resize-detector'
 import Spinner from 'react-bootstrap/Spinner'
 
 const ResponsivePlot = (props) => {
+  const { width, height, ref } = useResizeDetector({})
   const { data, layout, config, style, isLoading } = props
   return (
     <>
       {isLoading ? (
         <Spinner animation='border' role='status' />
       ) : (
-        <Plot
-          data={data}
-          layout={layout}
-          config={config}
-          useResizeHandler={true}
-          style={style}
-        />
+        <div ref={ref} style={{ display: 'flex', height: '100%' }}>
+          <Plot
+            data={data}
+            layout={{...layout, width, height}}
+            config={config}
+            useResizeHandler={true}
+            style={style}
+          />
+        </div>
       )}
     </>
   )
