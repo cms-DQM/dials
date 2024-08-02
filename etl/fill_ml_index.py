9e8826c0623537bcb0b582435bc76264281b56c2
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-08-02 14:53:40 +0200

    chore: update fill ml index script with newer models and remove thr entry

--- etl/fill_ml_index.py
+++ etl/fill_ml_index.py
@@ -18,7 +18,6 @@ def register_model(ws, model_metadata):
         model = DimMLModelsIndex(
             filename=model_metadata["filename"],
             target_me=model_metadata["target_me"],
-            thr=model_metadata["thr"],
             active=model_metadata["active"],
         )
         session.add(model)
@@ -30,61 +29,101 @@ if __name__ == "__main__":
         {
             "filename": "model_CHFrac_highPt_Barrel_checkpoint_20240517.onnx",
             "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_highPt_Barrel",
-            "thr": 0.05,
-            "active": True,
+            "active": False,
         },
         {
             "filename": "model_CHFrac_highPt_EndCap_checkpoint_20240517.onnx",
             "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_highPt_EndCap",
-            "thr": 0.05,
-            "active": True,
+            "active": False,
         },
         {
             "filename": "model_CHFrac_lowPt_Barrel_checkpoint_20240517.onnx",
             "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_lowPt_Barrel",
-            "thr": 0.05,
-            "active": True,
+            "active": False,
         },
         {
             "filename": "model_CHFrac_lowPt_EndCap_checkpoint_20240517.onnx",
             "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_lowPt_EndCap",
-            "thr": 0.05,
-            "active": True,
+            "active": False,
         },
         {
             "filename": "model_CHFrac_mediumPt_Barrel_checkpoint_20240517.onnx",
             "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_mediumPt_Barrel",
-            "thr": 0.05,
-            "active": True,
+            "active": False,
         },
         {
             "filename": "model_CHFrac_mediumPt_EndCap_checkpoint_20240517.onnx",
             "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_mediumPt_EndCap",
-            "thr": 0.05,
-            "active": True,
+            "active": False,
         },
         {
             "filename": "model_MET_2_checkpoint_20240517.onnx",
             "target_me": "JetMET/MET/pfMETT1/Cleaned/MET_2",
-            "thr": 0.05,
-            "active": True,
+            "active": False,
         },
         {
             "filename": "model_METPhi_checkpoint_20240517.onnx",
             "target_me": "JetMET/MET/pfMETT1/Cleaned/METPhi",
-            "thr": 0.05,
-            "active": True,
+            "active": False,
         },
         {
             "filename": "model_METSig_checkpoint_20240517.onnx",
             "target_me": "JetMET/MET/pfMETT1/Cleaned/METSig",
-            "thr": 0.05,
-            "active": True,
+            "active": False,
         },
         {
             "filename": "model_SumET_checkpoint_20240517.onnx",
             "target_me": "JetMET/MET/pfMETT1/Cleaned/SumET",
-            "thr": 0.05,
+            "active": False,
+        },
+        {
+            "filename": "model_CHFrac_highPt_Barrel_checkpoint_20240720.onnx",
+            "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_highPt_Barrel",
+            "active": True,
+        },
+        {
+            "filename": "model_CHFrac_highPt_EndCap_checkpoint_20240720.onnx",
+            "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_highPt_EndCap",
+            "active": True,
+        },
+        {
+            "filename": "model_CHFrac_lowPt_Barrel_checkpoint_20240720.onnx",
+            "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_lowPt_Barrel",
+            "active": True,
+        },
+        {
+            "filename": "model_CHFrac_lowPt_EndCap_checkpoint_20240720.onnx",
+            "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_lowPt_EndCap",
+            "active": True,
+        },
+        {
+            "filename": "model_CHFrac_mediumPt_Barrel_checkpoint_20240720.onnx",
+            "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_mediumPt_Barrel",
+            "active": True,
+        },
+        {
+            "filename": "model_CHFrac_mediumPt_EndCap_checkpoint_20240720.onnx",
+            "target_me": "JetMET/Jet/Cleanedak4PFJetsCHS/CHFrac_mediumPt_EndCap",
+            "active": True,
+        },
+        {
+            "filename": "model_MET_2_checkpoint_20240720.onnx",
+            "target_me": "JetMET/MET/pfMETT1/Cleaned/MET_2",
+            "active": True,
+        },
+        {
+            "filename": "model_METPhi_checkpoint_20240720.onnx",
+            "target_me": "JetMET/MET/pfMETT1/Cleaned/METPhi",
+            "active": True,
+        },
+        {
+            "filename": "model_METSig_checkpoint_20240720.onnx",
+            "target_me": "JetMET/MET/pfMETT1/Cleaned/METSig",
+            "active": True,
+        },
+        {
+            "filename": "model_SumET_checkpoint_20240720.onnx",
+            "target_me": "JetMET/MET/pfMETT1/Cleaned/SumET",
             "active": True,
         },
     ]
