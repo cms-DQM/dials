a20863569e778d3c4edc6ccbc78d99ea91462ed4
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-11-19 17:13:33 +0100

    feat: add FieldsFilterMixin
    - Custom mixin to filter fields from ModelSerializer on the fly

--- backend/utils/serializers_mixins/__init__.py
+++ backend/utils/serializers_mixins/__init__.py
@@ -1,8 +1,10 @@
 from .dataset_name import DatasetNameMixin
+from .fields import FieldsFilterMixin
 from .me_name import MENameMixin
 
 
 __all__ = [
     "DatasetNameMixin",
+    "FieldsFilterMixin",
     "MENameMixin",
 ]
