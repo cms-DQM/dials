9ebbdc2ddcb823a6e2a2968df30ebb223b792b8b
Gabriel Moreira <gabrielmscampos@gmail.com>
2025-01-28 12:04:36 +0100

    fix: catch 404 http error when querying CAF

--- backend/caf/viewsets.py
+++ backend/caf/viewsets.py
@@ -1,7 +1,9 @@
 from typing import ClassVar
 
+from requests.exceptions import HTTPError
 from rest_framework import viewsets
 from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from utils.rest_framework_cern_sso.authentication import (
     CERNKeycloakClientSecretAuthentication,
@@ -20,6 +22,14 @@ class CAFViewSet(viewsets.ViewSet):
     def list(self, request):
         class_name = request.query_params.get("class_name")
         kind = request.query_params.get("kind")
-        caf = CAF(class_name, kind)
-        response = {**caf.latest, "content": caf.download(latest=True)}
+
+        try:
+            caf = CAF(class_name, kind)
+            response = {**caf.latest, "content": caf.download(latest=True)}
+        except HTTPError as err:
+            if "404 Client Error" in str(err):
+                raise NotFound(detail="CAF entry for given class_name and kind could not be found") from err
+            else:
+                raise err
+
         return Response(response)
