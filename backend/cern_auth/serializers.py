84d0359d3d839b40e718d6e76515ba42670f06ff
Gabriel Moreira <gabrielmscampos@gmail.com>
2025-01-29 15:40:12 +0100

    refactor: remove exchange_token endpoint
    This commit removes the `exchange-token` endpoint from the `cern_auth`
    router, that was used only by the frontend to fetch a token for the
    backend application client of the dials api.
    
    Since this endpoint was also resolving the user default workspace, a new
    GET endpoint was added `use-default-workspace`. Therefore the frontend
    can still automatically configure the user pre-selected workpspace based
    on his e-groups configured on the backend application client.

--- backend/cern_auth/serializers.py
+++ backend/cern_auth/serializers.py
@@ -1,18 +1,6 @@
 from rest_framework import serializers
 
 
-class ExchangedTokenSerializer(serializers.Serializer):
-    access_token = serializers.CharField()
-    expires_in = serializers.IntegerField()
-    refresh_expires_in = serializers.IntegerField()
-    refresh_token = serializers.CharField()
-    token_type = serializers.CharField()
-    id_token = serializers.CharField()
-    session_state = serializers.CharField()
-    scope = serializers.CharField()
-    default_workspace = serializers.CharField()
-
-
 class DeviceSerializer(serializers.Serializer):
     device_code = serializers.CharField()
     verification_uri_complete = serializers.CharField()
